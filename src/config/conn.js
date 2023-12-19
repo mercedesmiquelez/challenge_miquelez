a47e8aa098ea2400e261cc45c02e7a083a4b5c15
Mercedes Miquelez <mer_m_@hotmail.com>
2023-12-18 22:05:42 -0300

    Modificaciones

--- src/config/conn.js
+++ src/config/conn.js
@@ -1,13 +1,10 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-/*
- * Creamos un pool de conexiones
- */
 
 const pool = mysql.createPool({
   host: process.env.HOST,
-  user: process.env.USER,
+  user: 'root',
   password: process.env.DBPASS,
   database: process.env.DB,
   port: 3306,
@@ -16,9 +13,6 @@ const pool = mysql.createPool({
   queueLimit: 0,
 });
 
-/**
- * Testeamos que la conexión sea exitosa
- */
 
 pool.getConnection((error, connection) => {
   if (error) {
